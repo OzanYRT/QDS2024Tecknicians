c2d4e6d26b6363d71bdfa037d357fb24869c08f1
ozany <ozanyurtiisigi@gmail.com>
2024-03-10 07:48:27 -0700

    added apply link to saved cards

--- client/src/components/scholarshipCard.js
+++ client/src/components/scholarshipCard.js
@@ -7,6 +7,7 @@ export default function ScholarshipCard({ scholarship }) {
       <h3>{scholarship.name}</h3>
       <p>Amount: {scholarship.amount}</p>
       <p>Deadline: {scholarship.deadline}</p>
+      <p>Apply here: <a href={scholarship.link} target="_blank" rel="noopener noreferrer">{scholarship.link}</a></p>
     </div>
   );
 }
