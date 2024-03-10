550cc1c2c945828b89378496956c2adf67cbae19
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2024-03-09 23:58:13 -0800

    more css , centering stuff

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../style/LandingPage.css"; // Assuming this is the correct path to your CSS file
-import newLogo from '../assets/images/glow_6.png';
-
+import "../style/LandingPage.css"; 
+import newLogo from '../assets/images/glow_6.png'; 
 
 const LandingPage = () => {
   return (
-      <div className="text-center landing-page">
-          <div className="icon">
-              <img src={newLogo} alt="logo" className="logo"/>
-          </div>
-          <div className="d-grid gap-2 col-6 mx-auto">
-              <Link to="/login" className="btn btn-primary btn-custom">Login</Link>
-              <Link to="/register" className="btn btn-secondary btn-custom">Register</Link>
-          </div>
+    <div className="landing-page">
+      <div className="icon">
+        <img src={newLogo} alt="logo" className="logo" />
       </div>
+      <div className="d-grid gap-2 col-6 mx-auto">
+        <Link to="/login" className="btn btn-primary btn-custom">Login</Link>
+        <Link to="/register" className="btn btn-secondary btn-custom">Register</Link>
+      </div>
+    </div>
   );
 };
 
