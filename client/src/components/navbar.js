accc55e2dd8169ef9eaf7d900e7bb5770e3ef8f7
ssidhu271 <ssidhu271@my.bcit.ca>
2024-03-09 22:12:32 -0800

    Made changes to fix the git merge errors that had occurred.

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -17,10 +17,16 @@ export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand" to="/">
+        <NavLink className="navbar-brand" to="/home">
           <img style={{ width: '100%' }} src={newLogo} alt="logo" />
         </NavLink>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
